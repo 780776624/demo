2d440b051e9ec8b9c5f54a9a592a8c07742bb61c
780776624 <780776624@qq.com>
2018-09-08 00:30:56 +0800

    springboot+ssm+mysql 前后端分离

---
+++ demo1/src/main/java/com/accp/controller/TestController.java
@@ -0,0 +1,15 @@
+package com.accp.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin
+public class TestController {
+
+    @RequestMapping("test")
+    public String test(){
+        return "ok";
+    }
+}
